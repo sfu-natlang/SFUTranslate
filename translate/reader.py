a3a1d2e0678eb9e154dfc277bf01ff4a10ac764e
sshavara <sshavara@cs-nll-22.cs.sfu.ca>
2019-12-25 01:08:06 -0800

    IWSLT17 is loaded through torchtext

--- translate/reader.py
+++ translate/reader.py
@@ -36,14 +36,15 @@ class IWSLT(datasets.TranslationDataset):
     This one just does not apply filter_pred designed for length limiting to the test and dev datasets.
     The IWSLT 2016 TED talk translation task"""
 
-    base_url = 'https://wit3.fbk.eu/archive/2016-01//texts/{}/{}/{}.tgz'
+    #base_url = 'https://wit3.fbk.eu/archive/2016-01//texts/{}/{}/{}.tgz'
+    base_url = 'https://wit3.fbk.eu/archive/2017-01-trnted/texts/{}/{}/{}-{}.tgz'
     name = 'iwslt'
     base_dirname = '{}-{}'
 
     @classmethod
     def splits(cls, exts, fields, root='.data',
-               train='train', validation='IWSLT16.TED.tst2013',
-               test='IWSLT16.TED.tst2014', **kwargs):
+               train='train', validation='IWSLT16.TED.dev2010',
+               test='IWSLT17.TED.tst2015', **kwargs):
         debug_mode = False
         if "debug_mode" in kwargs:
             debug_mode = kwargs["debug_mode"]
@@ -151,12 +152,12 @@ if bool(cfg.debug_mode):
 else:
     train, val, test = IWSLT.splits(
         filter_pred=lambda x: len(vars(x)['src']) <= cfg.max_sequence_length and len(
-            vars(x)['trg']) <= cfg.max_sequence_length, exts=('.de', '.en'), fields=(SRC, TGT),
+            vars(x)['trg']) <= cfg.max_sequence_length, exts=('.de', '.en'), fields=(SRC, TGT),test='IWSLT17.TED.tst2015', validation='IWSLT17.TED.dev2010',
         debug_mode=bool(cfg.debug_mode))
-    src_val_file_address = ".data/iwslt/{0}-{1}/IWSLT16.TED.tst2013.{0}-{1}.{0}".format(src_lan, tgt_lan)
-    tgt_val_file_address = ".data/iwslt/{0}-{1}/IWSLT16.TED.tst2013.{0}-{1}.{1}".format(src_lan, tgt_lan)
-    src_test_file_address = ".data/iwslt/{0}-{1}/IWSLT16.TED.tst2014.{0}-{1}.{0}".format(src_lan, tgt_lan)
-    tgt_test_file_address = ".data/iwslt/{0}-{1}/IWSLT16.TED.tst2014.{0}-{1}.{1}".format(src_lan, tgt_lan)
+    src_val_file_address = ".data/iwslt/{0}-{1}/IWSLT17.TED.dev2010.{0}-{1}.{0}".format(src_lan, tgt_lan)
+    tgt_val_file_address = ".data/iwslt/{0}-{1}/IWSLT17.TED.dev2010.{0}-{1}.{1}".format(src_lan, tgt_lan)
+    src_test_file_address = ".data/iwslt/{0}-{1}/IWSLT17.TED.tst2015.{0}-{1}.{0}".format(src_lan, tgt_lan)
+    tgt_test_file_address = ".data/iwslt/{0}-{1}/IWSLT17.TED.tst2015.{0}-{1}.{1}".format(src_lan, tgt_lan)
 
 
 print("Number of training examples: {}".format(len(train.examples)))
