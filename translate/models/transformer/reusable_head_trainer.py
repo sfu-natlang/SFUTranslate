211e3915dfe1409bd847291105e458269e7688dc
hassan shavarani <sshavara@sfu.ca>
2020-05-20 12:41:42 -0700

    minor change

--- translate/models/transformer/reusable_head_trainer.py
+++ translate/models/transformer/reusable_head_trainer.py
@@ -193,7 +193,7 @@ def extract_word_boundaries(bis_array):
             if latest_multipart_word_start != -1 and latest_multipart_word_end != -1:
                 word_boundaries.append((latest_multipart_word_start, latest_multipart_word_end))
             elif latest_multipart_word_end == -1:
-                print("WARNING: latest_multipart_word_end was not found when the next new word began")
+                print("WARNING: latest_multipart_word_end was not found when the next single word began")
             # print("Single token word from [{}-{}]".format(idx, idx+1))
             word_boundaries.append((idx, idx+1))
             latest_multipart_word_start = -1
